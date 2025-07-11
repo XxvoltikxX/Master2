b13218b73ec724c89c1c1b152450e07370a3dad6
XxvoltikxX <voltikgameryt@onet.pl>
2025-07-11 23:42:08 +0200

    Add files via upload

---
+++ webmanifest.php
@@ -0,0 +1,13 @@
+{
+    "name": "MasterOf13FPS",
+    "short_name": "MasterOf13FPS",
+    "description": "MasterOf13FPS Forum",
+    "lang": "en-US",
+    "dir": "ltr",
+    "display": "standalone",
+    "scope": "\/forum\/",
+    "id": "\/forum\/?_pwa=1",
+    "start_url": "\/forum\/?_pwa=1",
+    "background_color": "#182139",
+    "theme_color": "#272e48"
+}
